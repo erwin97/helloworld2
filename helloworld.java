a03b3339108f599c4efe48735d456a0a0e15ad65
erwin97 <49305500+erwin97@users.noreply.github.com>
2019-04-07 16:11:53 +0200

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        while(true){
+            System.out.println("Hello World!");
+        }
     }
 }
